1aa239c6c054c4736c5bd237e8872c474e292d20
Sai-crypto125 <1118373@apps.nsd.org>
2025-09-17 15:50:55 +0000

    Done

--- src/main/java/org/nhs/app/App.java
+++ src/main/java/org/nhs/app/App.java
@@ -1,8 +1,7 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+class main 
+{
+public static void main(String[] args){
+    System.out.println("Hello World");
+    }
+}
 
-package org.nhs.app;
-
-// Your code goes here
